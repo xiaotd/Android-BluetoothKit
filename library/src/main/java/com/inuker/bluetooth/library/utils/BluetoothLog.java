0a91e929bc9c3978464902529d2d45c0ee4a31ed
xiaotd <wscem@163.com>
2019-04-11 14:11:07 +0800

    Update BluetoothLog.java
    add showLog flag

--- library/src/main/java/com/inuker/bluetooth/library/utils/BluetoothLog.java
+++ library/src/main/java/com/inuker/bluetooth/library/utils/BluetoothLog.java
@@ -12,25 +12,45 @@ import java.io.Writer;
 public class BluetoothLog {
 
     private static final String LOG_TAG = "miio-bluetooth";
+    private static boolean showLog = false;
+    public static void setShowLog(boolean show){
+        showLog = show;
+    }
 
     public static void i(String msg) {
-        Log.i(LOG_TAG, msg);
+        if(showLog){
+            Log.i(LOG_TAG, msg);
+        }
     }
 
     public static void e(String msg) {
         Log.e(LOG_TAG, msg);
     }
+    public static void e(String msg,Throwable e) {
+        Log.e(LOG_TAG, msg,e);
+    }
 
     public static void v(String msg) {
-        Log.v(LOG_TAG, msg);
+        if(showLog){
+            Log.v(LOG_TAG, msg);
+        }
     }
 
     public static void d(String msg) {
-        Log.d(LOG_TAG, msg);
+        if(showLog){
+            Log.d(LOG_TAG, msg);
+        }
     }
 
     public static void w(String msg) {
-        Log.w(LOG_TAG, msg);
+        if(showLog){
+            Log.w(LOG_TAG, msg);
+        }
+    }
+    public static void w(String msg,Throwable e) {
+        if(showLog){
+            Log.w(LOG_TAG, msg,e);
+        }
     }
 
     public static void e(Throwable e) {
